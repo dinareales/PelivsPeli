182713cb3b423518d59fb0e7baac829cc929d4d2
Dina <dinamar582@hotmail.com>
2019-09-05 18:22:43 -0500

    primer commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var controller = require('./controladores/controlador');
+
+var app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(bodyParser.json());
+
+//seteamos el puerto en el cual va a escuchar los pedidos la aplicación
+var puerto = '8080';
+
+app.listen(puerto, function () {
+  console.log( "Escuchando en el puerto " + puerto );
+});
